e641598796983a0cc2328914e419eb41525f7b56
Manahil-Abid <manahilabid990@gmail.com>
2024-10-04 17:23:43 +0500

    Add files via upload

---
+++ New folder (2)/app.js
@@ -0,0 +1,96 @@
+                                      // class 6 // JavaScript
+//comparison Operator : Comparison operators are used in logical statements to determine equality or difference between variables or values.
+// == Equal to , === Equal value and equal type :
+                                     //Excercise (practice comparison operator)
+// Is X=10 is equal to 12?
+// Equal to;
+//  let x=10;
+//  console.log(x==12); //false , No X value is 10 not 12 
+//  1. let x=10;
+//  console.log(x=="10"); //true ,equal to
+// Explanation: the log value is in "string" and X= 10 but the answer will be true because in equal to (==) it's ignore data type.
+// 2.  let x=10;
+//  console.log(x==="10") //False ,Equal value and equal type 
+// Explanation: In equal value and equal type it consider Data type , X=10 (Number) & in log the value (String).
+//3. let x = 15;
+// console.log(x== 15); //true ,equal to
+// console.log(x==7+8); //true ,equal to
+// Explanation: answer is true because when we put arthimetic operator (+) between operands they add it (7+8)=15 so (X==15) and x is already 15
+// console.log(x===18-3); // true , Equal value and equal type
+// 18-3= 15 and variable x is already 15
+// console.log(x==7+"8"); // false , equal to
+// Explanation: beacuse in log one value is in num and another is in string;
+// 4. let x = 15;
+// x++; // post-increment (15+1) = 16 
+// console.log(++x); // pre-increment (16+1)=17
+// console.log(x==17); // true , equal to
+//  5. let x= 15;
+//  x++; //16
+//  console.log(++x); //17
+//  console.log(x--,x++); // 16 ,17
+//  console.log(x==17);// 17 // true , equal to
+// 6. let x= 15;
+// x++; //16
+// console.log(++x); //17
+// console.log(x--,x++,x==17); //17 // true , equal to
+// CLASS TASKS;
+// let x=25;
+// x--; //post-decrement, 24
+// ++x; //pre-increment, 25
+// console.log(x++,--x); //  25 post-increment (26) ,pre-decrement 25
+// console.log(x--, x--); // post-decrement 24, post-decrement 23
+// console.log(x==25,x); // 23==25 , false
+// console.log(x===25,x); //23===25, false 
+
+                             //Not Equal To != (means isk bara nhi ha) (!= ignore data type)
+                             // Not equal to & type !==
+                              //Exercise
+// Assign 5 to n, and display the value in each point! 
+// let n = 5;
+// console.log(n+3,9-1); // 8 , 8 
+// console.log(n!=n); // false ,Not Equal To !
+
+// let n = 5;
+// console.log(n!=="5"); // true , Not equal to & type !==
+// console.log(n!=="8-3");  //true , Not equal to & type !==
+
+// less than <
+// less than or equal to <=
+// greater than >
+// less than or equal to <=
+
+// Assign 8 to a, and display the value which (a=<,>,<==):
+// let a=8;
+// console.log(a<27); // true
+// console.log(27<a); // false
+// console.log(a<=8); // true
+// console.log(a<="8"); // true "<= ignore datatype"
+// console.log(a>99); //false
+// console.log(a>8); // false
+
+// Assign 5 to x, and display the value of the comparison (x !== 5):
+// let x = 5;
+// console.log(x !== 5); // false
+
+// what will the value of ==,!==,===?
+// console.log(7==7, 8!==8,9==="9"); // true , false , false
+
+// what will the value of false?
+// console.log(false == 1-1+1-1); // true ,false value is already 0;
+
+// what will the value of false?
+// console.log(false !== 4-1+1-1); // true 
+
+// logic building!!
+// console.logundefined==false // false
+// null=false // false
+// undefined == 0 // false
+// null == 0 //false
+// undefined == "" //false
+// null ='' // false
+// false == ''  // true
+// null == undefined // true It means null is equal to undefined but not identical.When we define a variable to undefined then we are trying to convey that the variable does not exist . When we define a variable to null then we are trying to convey that the variable is empty.
+
+
+
+
