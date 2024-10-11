da8be708a882da618ecd36d0d833cde7a441065e
Manahil-Abid <manahilabid990@gmail.com>
2024-10-11 17:02:55 +0500

    Update app.js

--- New folder (2)/app.js
+++ New folder (2)/app.js
@@ -241,6 +241,94 @@
 // else {
 //     console.log("its a odd");
 // }
+// Class # 09 //
+// 1. Write a JavaScript program that asks the user for their days and then displays a message that indicating the days //
+// let days = prompt("Enter a day");
+// if ( days == 1){
+//     console.log("Monday")
+// }
+// else if ( days == 2){
+//     console.log("Tuesday")
+// }
+// else if ( days == 3){
+//     console.log("Wednesday")
+// }
+// else if ( days == 4){
+//     console.log("Thursday")
+// }
+// else if ( days == 5){
+//     console.log("Friday")
+// }
+// else if ( days == 6){
+//     console.log("Saturday")
+// }
+// else if ( days == 7){
+//     console.log("Sunday")
+// }
+// else{
+//     console.log("invalid day!!")
+// }
+// convert into Switch 
+// let days = prompt("Enter a day");
+// switch (days) {
+//     case "1":
+//     console.log("Monday");
+//         break;
+//     case "2":
+//     console.log("Tuesday");
+//             break;
+//     case "3":
+//     console.log("Wednesday");
+//             break;
+//     case "4":
+//     console.log("Thursday");
+//             break;
+//      case "5":
+//     console.log("Friday");
+//             break;
+//     case "6":
+//     console.log("saturday");
+//             break;
+//     case "7":
+//     console.log("sunday");
+//             break;
+//     default:
+//         console.log("Invalid day"); 
+//         break;
+// }
+//
+// let bevarage = prompt("what would you like to drink? \nWater \nSoda \nJuice \nTea \nShake \nCoffee");
+// switch (bevarage) {
+//     case "Water"&&"WATER"&&"water":
+//     console.log("Here your water");
+//         break;
+//     case "Soda":
+//     console.log("Here your Soda");
+//             break;
+//     case "Juice":
+//     console.log("Here your Juice");
+//             break;
+//     case "Tea":
+//     console.log("Here your Tea");
+//             break;
+//     case "Shake":
+//      console.log("Here your Shake");
+//             break;
+//     case "Coffee":
+//     console.log("Here your Coffee");
+//             break;
+//     default:
+//         console.log("Sorry !! Not Available"); 
+//         break;
+// }
+// Condition (Ternary) Operator
+// const age = 26; 
+// const beverage = age >= 21 ? "Beer" : "Juice";
+// console.log(beverage); // "Beer"
+// const age = 88;
+// const death = age > 90 ? "Your life is going to end soon": "Enjoy Your Life";
+// console.log(death);
+
   
 
 
